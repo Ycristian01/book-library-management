bd289faeaaaa3b134beeeef0acf1acc6a4681686
ycristian01 <ycristian@uninorte.edu.co>
2025-07-21 17:26:59 -0500

    feat: config apis and add script for auto deploy backend

---
+++ backend/src/main/java/com/library/api/TestResource.java
@@ -0,0 +1,16 @@
+package com.library.api;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class TestResource {
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello() {
+    return "Hello";
+  }
+}
